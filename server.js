1bdf472bc67255906715351d1f17a30782c72cfd
Hilmi <hilmi@gementar.com>
2024-02-21 10:01:36 +0800

    Inclide cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const cors = require('cors');
 const app = express();
 const path = require('path');
 const fs = require('fs');
